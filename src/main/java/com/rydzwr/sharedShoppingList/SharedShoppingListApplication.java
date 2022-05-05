552f415969b76b3ec76f81e33e5a1a4218c247e6
rydzwr <rydzwr@gmail.com>
2022-05-05 17:15:15 +0200

    BUILD: Project

---
+++ src/main/java/com/rydzwr/sharedShoppingList/SharedShoppingListApplication.java
@@ -0,0 +1,13 @@
+package com.rydzwr.sharedShoppingList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharedShoppingListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharedShoppingListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rydzwr/sharedShoppingList/SharedShoppingListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rydzwr.sharedShoppingList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharedShoppingListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
